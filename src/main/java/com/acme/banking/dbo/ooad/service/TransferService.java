3a186133ac79b8f5a794a4580494a2fb74fa4bc0
Daria <greattoster@gmail.com>
2019-06-21 10:22:13 +0300

    AccountRepository.java refactoring

--- src/main/java/com/acme/banking/dbo/ooad/service/TransferService.java
+++ src/main/java/com/acme/banking/dbo/ooad/service/TransferService.java
@@ -1,10 +1,22 @@
 package com.acme.banking.dbo.ooad.service;
 
+import com.acme.banking.dbo.ooad.dal.AccountRepository;
 import com.acme.banking.dbo.ooad.domain.Account;
 
 public class TransferService {
-    public void transfer(Account from, Account to, double amount) {
-        from.withdraw(amount);
-        to.deposit(amount);
+
+    private AccountRepository repository;
+
+    public TransferService(AccountRepository repository) {
+        this.repository = repository;
+    }
+
+    public void transfer(long from, long to, double amount) {
+        Account withdrawn=repository.findById(from);
+        Account received=repository.findById(to);
+        withdrawn.withdraw(amount);
+        received.deposit(amount);
+        repository.save(withdrawn);
+        repository.save(received);
     }
 }
--- src/test/java/com/acme/banking/dbo/ooad/TransferServiceTest.java
+++ src/test/java/com/acme/banking/dbo/ooad/TransferServiceTest.java
@@ -1,5 +1,6 @@
 package com.acme.banking.dbo.ooad;
 
+import com.acme.banking.dbo.ooad.dal.AccountRepository;
 import com.acme.banking.dbo.ooad.domain.Account;
 import com.acme.banking.dbo.ooad.domain.SavingAccount;
 import com.acme.banking.dbo.ooad.service.TransferService;
@@ -11,18 +12,24 @@ public class TransferServiceTest {
     @Test
     public void shouldUpdateAccountsStateWhenTransfer() {
         //region Given
-        TransferService sut = new TransferService();
+        AccountRepository repo = mock(AccountRepository.class);
+
+        TransferService sut = new TransferService(repo);
         Account fromAccount = mock(Account.class);
         Account toAccount = mock(Account.class);
+        when(repo.findById(0L)).thenReturn(fromAccount);
+        when(repo.findById(1L)).thenReturn(toAccount);
         //endregion
 
         //region When
-        sut.transfer(fromAccount, toAccount, 100.);
+        sut.transfer(0, 1, 100.);
         //endregion
 
         //region Then
-        verify(fromAccount, times(1)).withdraw(100.);
-        verify(toAccount).deposit(anyDouble());
+        verify(fromAccount).withdraw(100.);
+        verify(toAccount).deposit(100);
+        verify(repo).save(fromAccount);
+        verify(repo).save(toAccount);
         //endregion
     }
 }
